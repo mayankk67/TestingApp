1bd861fad98d81a45bc79519edc8fd78588033c0
mak kumar <mak@gmail.com>
2019-11-03 13:49:31 +0530

    Updates

---
+++ precache-manifest.68c61770e20ec4ab0136f7994a25694e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74789663c89940e247422b3d17209128",
+    "url": "/TestingApp/index.html"
+  },
+  {
+    "revision": "e34ea21a80a58eb5684c",
+    "url": "/TestingApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "999f97057801d59d34f7",
+    "url": "/TestingApp/static/js/2.eb3c2c58.chunk.js"
+  },
+  {
+    "revision": "e34ea21a80a58eb5684c",
+    "url": "/TestingApp/static/js/main.d889c886.chunk.js"
+  },
+  {
+    "revision": "f31e5f000efac0654f7a",
+    "url": "/TestingApp/static/js/runtime-main.61394482.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TestingApp/static/media/logo.25bf045c.svg"
+  }
+]);
